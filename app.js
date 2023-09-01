6ecbc56312a1b3bb17344f6f07ed644a54787b48
Sreejit Guha Thakurta <85695034+Sreejit2001@users.noreply.github.com>
2023-09-01 13:51:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+function bms() {
+    document.location.href = "https://drive.google.com/file/d/1M8v_DOuFE2TmwdTckizEDwvI---zIVRw/view?usp=sharing"
+}
+
+function foodie() {
+    document.location.href = "https://food-ie.netlify.app"
+}
+function hdp() {
+    document.location.href = "https://colab.research.google.com/drive/1l0f7OzoMZESmDpiCdKYg-ny-Li9g1Kg0?usp=sharing"
+}
+function todo() {
+    document.location.href = "https://sreejit2001.github.io/To-Do-App/"
+}
+function calculator() {
+    document.location.href = "https://sreejit2001.github.io/calculator/"
+}
+function portfolio() {
+    document.location.href = "https://sreejit2001.github.io/portfolio/"
+}
+
+function resume() {
+    document.location.href = "https://drive.google.com/file/d/1C6360MgiB_vY6hUG_XhhNDgLkEFAhP1r/view?usp=sharing"
+}
+function contact() {
+    document.location.href = "#contact_section"
+}
